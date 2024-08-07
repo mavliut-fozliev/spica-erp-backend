6bab5c943e1792c120f0483559be3b6d6133c989
MayroYo <128398090+mayroyo@users.noreply.github.com>
2024-08-07 17:53:58 +0300

    feat: create function to get available projects and customers (#1)

---
+++ function/66b1ffe2ee419a002c1f2700/index.ts
@@ -0,0 +1,33 @@
+import * as Bucket from "@spica-devkit/bucket";
+import { env } from "../../6544faf5d71b6e002cf70b78/.build"
+
+export async function getAvailableProjectsAndCustomers(req, res) {
+	const jwt = req.headers.get('authorization');
+
+	Bucket.initialize({ identity: jwt })
+
+	const projectRelations = ["customer", "project.products.product", "products.product"]
+
+	const projects = await Bucket.data.getAll(env.PROJECT_BUCKET_ID, { queryParams: { filter: { status: "exist" }, relation: projectRelations } })
+
+	const offers = await Bucket.data.getAll(env.OFFERS_BUCKET_ID, { queryParams: { filter: { status: { $nin: ["deleted", "revised"] } } } })
+
+	const availableProjects = []
+	const availableCustomers = []
+
+	projects.forEach((project) => {
+		const currentProjectOffers = offers.filter(offer => offer.project == project._id)
+		const allOffersProducts = currentProjectOffers.flatMap(offer => offer.products)
+
+		if (project.products?.length > allOffersProducts.length)
+			availableProjects.push(project)
+
+		if (!availableCustomers.some(ac => ac._id == project.customer?._id)) {
+			availableCustomers.push(project.customer)
+		}
+	});
+
+	const resData = { availableProjects, availableCustomers }
+
+	return res.status(201).send(resData);
+}
