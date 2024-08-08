18dbe0f3fca372e467685bef22ba35d2da50940b
Spica <Spica>
2024-08-08 15:01:12 +0000

    "refactor: rename getAvailableOffers function"

--- function/66b4d593e6d086002c5dbeda/index.ts
+++ function/66b4d593e6d086002c5dbeda/index.ts
@@ -1,3 +1,9 @@
-export function getAvailableOffers(req, res) {
+import * as Bucket from "@spica-devkit/bucket";
+import { env } from "../../6544faf5d71b6e002cf70b78/.build"
+
+export async function getAvailableProjects(req, res) {
+	const jwt = req.headers.get('authorization');
+	Bucket.initialize({ identity: jwt })
+
 	return res.status(201).send([]);
 }
