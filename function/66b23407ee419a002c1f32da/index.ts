f557e2466cb95de0be55e58fdefb17465a4c3313
Spica <Spica>
2024-08-09 13:06:04 +0000

    "refactor: send maintenance notifications function"

--- function/66b23407ee419a002c1f32da/index.ts
+++ function/66b23407ee419a002c1f32da/index.ts
@@ -4,16 +4,12 @@ import { env } from "../../6544faf5d71b6e002cf70b78/.build";
 const maintenanceFaultsBucketId = env.MAINTENANCE_FAULTS_BUCKET_ID;
 const notificationBucketId = env.NOTIFICATION_BUCKET_ID;
 
+const maintenanceManagerId = "65291d7bffa6b3002d10dceb"
+
 function insertNotification(title, description, user) {
     try {
-        const notificationContent = {
-            title,
-            description,
-            user
-        };
-
+        const notificationContent = { title, description, user };
         Bucket.data.insert(notificationBucketId, notificationContent);
-        console.log(`Bildirim başarıyla eklendi: ${title}`);
     } catch (error) {
         console.error("Bildirim eklenirken hata oluştu", error);
     }
@@ -22,61 +18,107 @@ function insertNotification(title, description, user) {
 function updateFaultStatus(_id, updateData) {
     try {
         Bucket.data.patch(maintenanceFaultsBucketId, _id, updateData);
-        console.log(`Arıza durumu başarıyla güncellendi: ${_id}`);
     } catch (error) {
         console.error("Arıza durumu güncellenirken hata oluştu", error);
     }
 }
 
-async function sendMaintenanceNotifications(notificationPeriod, booleanName) {
+/**
+ * @param {number} notificationPeriod - Wait time in seconds
+ * @param {string} notificationState - Notification title
+*/
+async function sendMaintenanceNotifications(
+    notificationPeriod,
+    notificationState
+) {
     Bucket.initialize({ apikey: env.CLIENT_API_KEY });
 
-    const maintenanceFaults = await Bucket.data.getAll(maintenanceFaultsBucketId);
+    const strategy = notificationStrategyMap[notificationState];
+
+    if (!strategy) {
+        console.warn(`No strategy defined for: ${notificationState}`);
+        return;
+    }
+
     const now = Date.now();
 
+    const maintenanceFaults = await Bucket.data.getAll(maintenanceFaultsBucketId, {
+        queryParams: {
+            filter: {
+                $and: [
+                    { status: "exist" },
+                    { is_fixed: { $in: [false, undefined, null] } },
+                    { [notificationState]: { $in: [false, undefined, null] } }
+                ]
+            },
+            relation: ["maintenance_unit"]
+        }
+    });
+
     for (const fault of maintenanceFaults) {
-        const { _id, created_date, is_accepted, is_malfunction } = fault;
-        const startTime = new Date(created_date).getTime();
+        const { _id, maintenance_unit, updated_date, is_accepted } = fault;
+
+        const startTime = new Date(updated_date).getTime();
         const timeDifference = now - startTime;
 
-        if (!fault[booleanName] && timeDifference >= (notificationPeriod * 1000)) {
-            let notificationMessage = ""
-            switch (booleanName) {
-                case "notified":
-                    notificationMessage = is_accepted
-                        ? "Arıza kabul edildi!"
-                        : "Yeni arıza eklendi ama kabul edilmedi.";
+        const isWaitingTimePassed = timeDifference >= notificationPeriod * 1000;
 
-                    updateFaultStatus(_id, is_accepted ? { notified: true, created_date: new Date().toISOString() } : { notified: true });
-                    break;
+        if (!isWaitingTimePassed) continue;
 
-                case "first_reminder":
-                    if (!is_accepted || is_malfunction) continue
-                    notificationMessage = "Arıza kabul edildi ancak son 1 saat içerisinde arızanın bakımı yapılmadı!"
+        const result = strategy(is_accepted);
 
-                    updateFaultStatus(_id, { first_reminder: true });
-                    break;
+        if (!result) continue;
 
-                case "second_reminder":
-                    if (!is_accepted || is_malfunction) continue
-                    notificationMessage = "Arıza kabul edildi ancak son 2 saat içerisinde arızanın bakımı yapılmadı!"
+        updateFaultStatus(_id, result.status);
 
-                    updateFaultStatus(_id, { second_reminder: true });
-            }
+        const unitInfo = `Tesisat No: ${maintenance_unit.installation_number}. Bina Adı: ${maintenance_unit.building_name}.`
 
-            insertNotification(`Yeni Arıza: ${_id}`, notificationMessage, "65291d7bffa6b3002d10dceb");
-        }
+        insertNotification(`Yeni Arıza: ${unitInfo}`, result.message, maintenanceManagerId);
     }
 }
 
+const notificationStrategyMap = {
+    notified: (is_accepted) => {
+
+        const message = is_accepted
+            ? "Arıza kabul edildi!"
+            : "Yeni arıza eklendi ama kabul edilmedi.";
+
+        const status = is_accepted
+            ? { notified: true, updated_date: new Date().toISOString() }
+            : { notified: true };
+
+        return {
+            status,
+            message,
+        };
+    },
+    first_reminder: (is_accepted) => {
+        if (!is_accepted) return;
+
+        return {
+            message: "Arıza kabul edildi ancak son 1 saat içerisinde arızanın bakımı yapılmadı!",
+            status: { first_reminder: true },
+        };
+    },
+    second_reminder: (is_accepted) => {
+        if (!is_accepted) return;
+
+        return {
+            message: "Arıza kabul edildi ancak son 2 saat içerisinde arızanın bakımı yapılmadı!",
+            status: { second_reminder: true },
+        };
+    },
+};
+
 export function sendNotification() {
     const notificationSettings = [
-        { period: 600000, name: "notified" },         // 10 minutes
-        { period: 3600000, name: "first_reminder" },  // 1 hour
-        { period: 7200000, name: "second_reminder" }  // 2 hours
+        { period: 600, name: "notified" }, // 10 minutes
+        { period: 3600, name: "first_reminder" }, // 1 hour
+        { period: 7200, name: "second_reminder" }, // 2 hours
     ];
 
-    notificationSettings.forEach(setting => {
+    notificationSettings.forEach((setting) => {
         sendMaintenanceNotifications(setting.period, setting.name);
     });
 }
